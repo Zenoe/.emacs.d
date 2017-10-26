38e6452d746625b0c92fcb4997df6f598bdfe6b9
zenoe <gauss.cn@gamil.com>
2017-10-26 16:56:05 +0800

    misc.el

--- mysetting/misc.el
+++ mysetting/misc.el
@@ -1,15 +1,16 @@
 ;; font
 (set-frame-font "Inconsolata-12")
-(cond ((eq system-type 'darwin)  ( (lambda () (
-                                               (set-face-attribute 'default nil :font
-                                                                   (format "%s:pixelsize=%d" (car fonts) 12))
-                                               ))))
-            ((eq system-type 'gnu/linux)  (lambda () ()))
-            ((eq system-type 'windows-nt)  ((lambda () (let ((fonts '("Consolas"  "Microsoft Yahei")))
-                                                         (dolist (charset '(kana han symbol cjk-misc bopomofo))
-                                                           (set-fontset-font (frame-parameter nil 'font) charset
-                                                                             (font-spec :family (car (cdr fonts)))))
-                                                        )))))
+(setq ring-bell-function 'ignore)
+;; (cond ((eq system-type 'darwin)  ( (lambda () (
+;;                                                (set-face-attribute 'default nil :font
+;;                                                                    (format "%s:pixelsize=%d" (car fonts) 12))
+;;                                                ))))
+;;             ((eq system-type 'gnu/linux)  (lambda () ()))
+;;             ((eq system-type 'windows-nt)  ((lambda () (let ((fonts '("Consolas"  "Microsoft Yahei")))
+;;                                                          (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;                                                            (set-fontset-font (frame-parameter nil 'font) charset
+;;                                                                              (font-spec :family (car (cdr fonts)))))
+;;                                                         )))))
 ;; font end
 
 (eval-after-load 'evil-commands
